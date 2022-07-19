b9d24715fc6b14156edfc3e801950cf170aa136c
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-19 15:14:32 +0200

    use local package functions to run preprocessing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,107 +1,9 @@
-from sklearn.preprocessing import MinMaxScaler
-from pathlib import Path
-from scipy import stats
-import pandas as pd
-import numpy as np
+from src.utils.preprocessing import map_data
+from src.utils.preprocessing import clean_data
+from src.utils.preprocessing import run_preprocessing
+from src.utils.input import input_scan_list
 import argparse
-import glob
 import os
-
-def clean_data(df_to_clean):
-    df_to_clean = df_to_clean.copy()
-
-    # get range of features to clean
-    contiguous_columns = ['DurationTot', 'AveragePenPressureVar']
-    features_to_clean_range = np.r_[df_to_clean.columns.get_loc(contiguous_columns[0]):
-                                    df_to_clean.columns.get_loc(contiguous_columns[1]) + 1]
-    # create mask for all rows which only contain 0's
-    rows_to_drop_mask = df_to_clean.iloc[:, features_to_clean_range].eq(0).all(1)
-    # get list of indexes for rows that match mask
-    rows_to_drop = list(df_to_clean[rows_to_drop_mask].index.values)
-
-    # drop selected rows from raw dataframe
-    df_cleaned = df_to_clean.drop(rows_to_drop)
-    # # drop columns which only contain 0's
-    df_cleaned = df_cleaned.loc[:, (df_cleaned != 0).any(axis=0)]
-    # remove rows that have outliers in at least one column
-    df_cleaned = df_cleaned[(np.abs(stats.zscore(df_cleaned)) < 3).all(axis=1)]
-    # # drop columns which only contain 0's
-    df_cleaned = df_cleaned.loc[:, (df_cleaned != 0).any(axis=0)]
-
-    return df_cleaned
-
-def normalize_data(df_to_normalize):
-    df_to_normalize = df_to_normalize.copy()
-    df_normalized = df_to_normalize.copy()
-
-    # get range of features that need to be normalized
-    non_contiguous_columns = ['Age', 'Instruction']
-    contiguous_columns = ['DurationTot', 'NumOfStrokes']
-    features_to_normalize_range = np.r_[df_to_normalize.columns.get_indexer(non_contiguous_columns),
-                                        df_to_normalize.columns.get_loc(contiguous_columns[0]):
-                                        df_to_normalize.columns.get_loc(contiguous_columns[1]) + 1] 
-    # get features names from range of features
-    features_to_normalize = df_to_normalize.columns[features_to_normalize_range]
-    # initialize min max scaler and apply to dataframe to normalize
-    scaler = MinMaxScaler()
-    df_normalized[features_to_normalize] = scaler.fit_transform(df_to_normalize[features_to_normalize])
-    df_inverted = df_normalized.copy()
-    df_inverted[features_to_normalize] = scaler.inverse_transform(df_normalized[features_to_normalize])
-
-    return df_normalized
-
-def map_data(df_to_map):
-    df_to_map = df_to_map.copy() 
-    df_mapped = df_to_map.copy()
-
-    # mapping dictionaries for string to numeric features
-    sex_map = {'Maschile': 0, 'Femminile': 1}
-    work_map = {'Manuale': 0, 'Intellettuale': 1}
-    label_map = {'Sano': 0, 'Malato': 1}
-
-    # map dataframe with mapping dictionaries
-    df_mapped['Sex'] = df_to_map['Sex'].map(sex_map)
-    df_mapped['Work'] = df_to_map['Work'].map(work_map)
-    df_mapped['Label'] = df_to_map['Label'].map(label_map)
-
-    return df_mapped
-
-def build_data(input_path, output_path):
-    # read input path as dataframe
-    df_raw = pd.read_csv(input_path, sep=';', converters={'Sex': str.strip,
-                                                          'Work': str.strip,
-                                                          'Label': str.strip})
-    # entire preprocessing
-    df_mapped = map_data(df_raw)
-    df_cleaned = clean_data(df_mapped)
-    df_normalized = normalize_data(df_cleaned)
-
-    output_dirname = Path(os.path.dirname(output_path))
-    output_dirname.mkdir(parents=True, exist_ok=True)
-    df_normalized.to_csv(output_path, sep=';', index=False)
-
-def recursive_input_scan(input_root, output_root, input_paths=[], output_paths=[]):
-    # scan input root dir (find files and subdirs)
-    input_root_content = os.listdir(input_root)
-
-    for content in input_root_content:
-        # for each content inside input root dir, build content absolute paths
-        input_content_path = os.path.join(input_root, content)
-        output_content_path = os.path.join(output_root, content)
-        # if content is file in csv format, append absolute paths in path lists 
-        if (os.path.isfile(input_content_path) and
-            os.path.splitext(input_content_path)[1] == '.csv'):
-            input_paths.append(input_content_path)
-            output_paths.append(output_content_path)
-        # if content is dir, recursively call this function to scan dir content
-        elif (os.path.isdir(input_content_path) and
-              not(input_content_path.startswith('.'))):
-            # update root input path and root output path for recursive call
-            new_input_root = input_content_path
-            new_output_root = output_content_path
-            recursive_input_scan(new_input_root, new_output_root, input_paths, output_paths)
-
-    return sorted(input_paths), sorted(output_paths)
 
 def main():
     # set up parser and possible arguments
@@ -119,27 +21,26 @@ def main():
 
     # store parsed arguments
     args = parser.parse_args()
-    args = vars(args)
-    input_path = Path(args['i'])
-    output_path = Path(args['o'])
-
-    # check output argument validity by checking its extension:
-    # if input is file -> output must be file with csv extension
-    # if input is dir  -> output must be dir without any extension
-    output_path_extension = (os.path.splitext(output_path))[1]
+    input_path = args.i
+    output_path = args.o
 
     # if input argument is not an existing file or directory, raise exception
     if (not(os.path.isfile(input_path)) and not(os.path.isdir(input_path))):
         raise ValueError(str(input_path) + ' is neither an existing file nor directory')
  
+    # get output extension to check output path validity later
+    # if input is file -> output must be file with csv extension
+    # if input is dir  -> output must be dir without any extension
+    output_path_extension = (os.path.splitext(output_path))[1]
+
     # check if input argument points to file
     if (os.path.isfile(input_path)):
         # if output argument is not a valid path to csv file
         if (output_path_extension != '.csv'):
             raise ValueError(str(output_path) + ' is not a valid csv file path' )
 
-        # build specified input file
-        build_data(input_path, output_path)
+        # preprocess input file
+        run_preprocessing(input_path, output_path)
 
     # check if input argument points to directory
     if (os.path.isdir(input_path)):
@@ -147,12 +48,11 @@ def main():
         if (output_path_extension != ''):
             raise ValueError(str(output_path) + ' is not a valid directory path')
 
-        # recursively scan input directory for any csv file 
-        # and store input/output path lists
-        input_paths, output_paths = recursive_input_scan(input_path, output_path)
-        # for each input/output path pair, build input and export to output
+        # look for any csv file inside input path and store corresponding paths
+        input_paths, output_paths = input_scan_list(input_path, output_path)
+        # preprocess each file inside input dir
         for input_path, output_path in zip(input_paths, output_paths):
-            build_data(input_path, output_path)
+            run_preprocessing(input_path, output_path)
 
 if __name__ == '__main__':
     main()
