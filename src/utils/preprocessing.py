5497ab903fff6a08e93e825d14d1d45d1685418e
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-20 09:36:20 +0200

    code refactor

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -5,27 +5,29 @@ import pandas as pd
 import numpy as np
 import os
 
+
 def map_data(df_to_map):
     df_mapped = df_to_map.copy()
 
     # mapping dictionaries for string to numeric features
-    sex_map = {'Maschile': 0, 'Femminile': 1}
-    work_map = {'Manuale': 0, 'Intellettuale': 1}
-    label_map = {'Sano': 0, 'Malato': 1}
+    sex_map = {"Maschile": 0, "Femminile": 1}
+    work_map = {"Manuale": 0, "Intellettuale": 1}
+    label_map = {"Sano": 0, "Malato": 1}
 
     # map dataframe with mapping dictionaries
-    df_mapped['Sex'] = df_to_map['Sex'].map(sex_map)
-    df_mapped['Work'] = df_to_map['Work'].map(work_map)
-    df_mapped['Label'] = df_to_map['Label'].map(label_map)
+    df_mapped["Sex"] = df_to_map["Sex"].map(sex_map)
+    df_mapped["Work"] = df_to_map["Work"].map(work_map)
+    df_mapped["Label"] = df_to_map["Label"].map(label_map)
 
     return df_mapped
+
 
 def clean_data(df_to_clean):
     df_cleaned = df_to_clean.copy()
 
     # get range of features to clean
-    contiguous_columns = ['DurationTot', 'AveragePenPressureVar']
-    features_to_clean_range = np.r_[df_to_clean.columns.get_loc(contiguous_columns[0]):
+    contiguous_columns = ["DurationTot", "AveragePenPressureVar"]
+    features_to_clean_range = np.r_[df_to_clean.columns.get_loc(contiguous_columns[0]) : 
                                     df_to_clean.columns.get_loc(contiguous_columns[1]) + 1]
     # create mask for all rows which only contain 0's
     rows_to_drop_mask = df_to_clean.iloc[:, features_to_clean_range].eq(0).all(1)
@@ -43,15 +45,16 @@ def clean_data(df_to_clean):
 
     return df_cleaned
 
+
 def normalize_data(df_to_normalize):
     df_normalized = df_to_normalize.copy()
 
     # get range of features that need to be normalized
-    non_contiguous_columns = ['Age', 'Instruction']
-    contiguous_columns = ['DurationTot', 'NumOfStrokes']
+    non_contiguous_columns = ["Age", "Instruction"]
+    contiguous_columns = ["DurationTot", "NumOfStrokes"]
     features_to_normalize_range = np.r_[df_to_normalize.columns.get_indexer(non_contiguous_columns),
-                                        df_to_normalize.columns.get_loc(contiguous_columns[0]):
-                                        df_to_normalize.columns.get_loc(contiguous_columns[1]) + 1] 
+                                        df_to_normalize.columns.get_loc(contiguous_columns[0]) : 
+                                        df_to_normalize.columns.get_loc(contiguous_columns[1]) + 1]
     # get features names from range of features
     features_to_normalize = df_to_normalize.columns[features_to_normalize_range]
 
@@ -61,11 +64,12 @@ def normalize_data(df_to_normalize):
 
     return df_normalized, scaler
 
+
 def run_preprocessing(input_path):
     # read input path as dataframe
-    df_raw = pd.read_csv(input_path, sep=';', converters={'Sex': str.strip,
-                                                          'Work': str.strip,
-                                                          'Label': str.strip})
+    df_raw = pd.read_csv(input_path, sep=";", converters={"Sex": str.strip,
+                                                          "Work": str.strip,
+                                                          "Label": str.strip})
     # standard preprocessing
     df_mapped = map_data(df_raw)
     df_cleaned = clean_data(df_mapped)
