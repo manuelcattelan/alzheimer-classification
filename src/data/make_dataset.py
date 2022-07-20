901615d13f96e6f260dc95399e8784cb9c4e6628
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-20 09:26:20 +0200

    code refactor

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,19 +6,24 @@ from pathlib import Path
 import argparse
 import os
 
+
 def main():
     # set up parser and possible arguments
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--input',
-                        type=str,
-                        metavar='<input_file/dir>',
-                        help='input path to file or directory to process',
-                        required=True)
-    parser.add_argument('--output',
-                        type=str,
-                        metavar='<output_file/dir>',
-                        help='output path to file or directory where processed data is saved',
-                        required=True)
+    parser.add_argument(
+        "--input",
+        type=str,
+        metavar="<input_file/dir>",
+        help="input path to file or directory to process",
+        required=True,
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        metavar="<output_file/dir>",
+        help="output path to file or directory where processed data is saved",
+        required=True,
+    )
 
     # store parsed arguments
     args = parser.parse_args()
@@ -26,32 +31,32 @@ def main():
     output_path = args.output
 
     # if input argument is not an existing file or directory, raise exception
-    if (not(os.path.isfile(input_path)) and not(os.path.isdir(input_path))):
-        raise ValueError(str(input_path) + ' is neither an existing file nor directory')
- 
+    if not (os.path.isfile(input_path)) and not (os.path.isdir(input_path)):
+        raise ValueError(str(input_path) + " is neither an existing file nor directory")
+
     # get output extension to check output path validity later
     # if input is file -> output must be file with csv extension
     # if input is dir  -> output must be dir without any extension
     output_path_extension = (os.path.splitext(output_path))[1]
 
     # check if input argument points to file
-    if (os.path.isfile(input_path)):
+    if os.path.isfile(input_path):
         # if output argument is not a valid path to csv file
-        if (output_path_extension != '.csv'):
-            raise ValueError(str(output_path) + ' is not a valid csv file path' )
+        if output_path_extension != ".csv":
+            raise ValueError(str(output_path) + " is not a valid csv file path")
 
         # preprocess input file
         df_processed = run_preprocessing(input_path)
         # export processed df
         output_dirname = Path(os.path.dirname(output_path))
         output_dirname.mkdir(parents=True, exist_ok=True)
-        df_processed.to_csv(output_path, sep=';', index=False)
+        df_processed.to_csv(output_path, sep=";", index=False)
 
     # check if input argument points to directory
-    if (os.path.isdir(input_path)):
+    if os.path.isdir(input_path):
         # if output argument is not a valid path to directory
-        if (output_path_extension != ''):
-            raise ValueError(str(output_path) + ' is not a valid directory path')
+        if output_path_extension != "":
+            raise ValueError(str(output_path) + " is not a valid directory path")
 
         # look for any csv file inside input path and store corresponding paths
         input_paths, output_paths = scan_input_to_list(input_path, output_path)
@@ -61,7 +66,8 @@ def main():
             # export processed df
             output_dirname = Path(os.path.dirname(output_path))
             output_dirname.mkdir(parents=True, exist_ok=True)
-            df_processed.to_csv(output_path, sep=';', index=False)
+            df_processed.to_csv(output_path, sep=";", index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
