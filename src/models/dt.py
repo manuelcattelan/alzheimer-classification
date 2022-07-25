a67609084e9d473ed002c82f029ce9f8ee00286c
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-25 19:13:33 +0200

    use wrapper functions to organize code better

--- src/models/dt.py
+++ src/models/dt.py
@@ -1,15 +1,8 @@
-from src.utils.performance import compute_classifier_performance
-from src.utils.performance import compute_best_task_performance
-from src.utils.classification import run_classification
-from src.utils.parameters_tuning import param_distribution
-from src.utils.parameters_tuning import param_grid
-from src.utils.parameters_tuning import tune_classifier
-from src.utils.scan_input import scan_input_dir
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import RepeatedStratifiedKFold
-import json
-import pandas as pd
-import numpy as np
+from src.utils.parameters_tuning import dt_params
+from src.utils.classification import file_classification
+from src.utils.classification import dir_classification
 import argparse
 import os
 
@@ -87,69 +80,20 @@ def main():
             raise ValueError(
                     output_path + " is not a valid path to .png file"
                     )
-
         # define classifier and cross validator
-        dt = tree.DecisionTreeClassifier()
+        dt = DecisionTreeClassifier()
         cv = RepeatedStratifiedKFold(
                 n_splits=args.splits,
                 n_repeats=args.repeats
                 )
-        # read input file as dataframe
-        df = pd.read_csv(input_path, sep=";")
-        # if tune argument was defined
-        if args.tune:
-            # based on tune argument option, select corresponding parameters
-            params = param_grid if args.tune == "grid" else param_distribution
-            # run hyperparameter tuning
-            (dt,
-             tuning_best_params,
-             tuning_time) = tune_classifier(
-                    dt, cv, params, df, args.tune
-                    )
-        # run classification on file
-        (splits_cm,
-         splits_train_time,
-         splits_test_time) = run_classification(
-                dt, cv, df
-                )
-        # compute classifier performance
-        (task_performance,
-         task_train_time,
-         task_test_time) = compute_classifier_performance(
-                splits_cm, splits_train_time, splits_test_time
-                )
-        # print classification results
-        print(
-                "Classification results for {}:".format(
-                    input_path
-                    )
-                )
-        if args.tune:
-            # print time taken for hyperparameter tuning
-            print(
-                    "\tHyperparameters tuning:"
-                    "\n\t\t>>> Time taken: {:.3f}s".format(
-                        tuning_time
-                        )
-                    )
-        print(
-                "\tClassification time:"
-                "\n\t\t>>> Training: {:.3f}s"
-                "\n\t\t>>> Testing: {:.3f}s".format(
-                    task_train_time, task_test_time
-                    )
-                )
-        print(
-                "\tClassification performance:"
-                "\n\t\t>>> Accuracy: {:.1f}%"
-                "\n\t\t>>> Precision: {:.1f}%"
-                "\n\t\t>>> Recall: {:.1f}%"
-                "\n\t\t>>> F1 Score: {:.1f}%".format(
-                    task_performance[0],
-                    task_performance[1],
-                    task_performance[2],
-                    task_performance[3]
-                    )
+        # run classification process on input file
+        file_classification(
+                dt,
+                cv,
+                input_path,
+                output_path,
+                dt_params,
+                args.tune
                 )
 
     # check if input argument points to directory
@@ -159,207 +103,22 @@ def main():
             raise ValueError(
                     output_path + " is not a valid directory path"
                     )
-
         # define classifier and cross validator
-        dt = tree.DecisionTreeClassifier()
+        dt = DecisionTreeClassifier()
         cv = RepeatedStratifiedKFold(
                 n_splits=args.splits,
                 n_repeats=args.repeats
                 )
-        # get input file path and build corresponding output file path
-        # of all files inside input directory
-        (input_paths,
-         output_paths) = scan_input_dir(
-                input_path, output_path
+        # run classification process on input directory
+        dir_classification(
+                dt,
+                cv,
+                input_path,
+                output_path,
+                dt_params,
+                args.tune,
+                args.metric
                 )
-        # for each directory found while traversing input dir
-        for input_dirpath, output_dirpath in zip(
-                sorted(input_paths),
-                sorted(output_paths)):
-
-            # list of paths of files inside currently considered dir
-            input_filepaths = sorted(input_paths[input_dirpath])
-            output_filepaths = sorted(output_paths[output_dirpath])
-
-            # if there's only one file inside the current dir,
-            # run single file classification
-            if len(input_filepaths) == 1:
-                # read input file as dataframe
-                df = pd.read_csv(input_filepaths[0], sep=";")
-                if args.tune:
-                    # based on tune argument option, select corresponding parameters
-                    params = param_grid if args.tune == "grid" else param_distribution
-                    # run hyperparameter tuning
-                    (dt,
-                     tuning_best_params,
-                     tuning_time) = tune_classifier(
-                            dt, cv, params, df, args.tune
-                            )
-                # run classification on file
-                (splits_cm,
-                 splits_train_time,
-                 splits_test_time) = run_classification(
-                        dt, cv, df
-                        )
-                # compute classifier performance
-                (task_performance,
-                 task_train_time,
-                 task_test_time) = compute_classifier_performance(
-                        splits_cm, splits_train_time, splits_test_time
-                        )
-                # print classification results
-                print(
-                        "Classification results for {}:".format(
-                            input_path
-                            )
-                        )
-                if args.tune:
-                    # print time taken for hyperparameter tuning
-                    print(
-                            "\tHyperparameters tuning:"
-                            "\n\t\t>>> Time taken: {:.3f}s".format(
-                                tuning_time
-                                )
-                            )
-                print(
-                        "\tClassification time:"
-                        "\n\t\t>>> Training: {:.3f}s"
-                        "\n\t\t>>> Testing: {:.3f}s".format(
-                            task_train_time, task_test_time
-                            )
-                        )
-                print(
-                        "\tClassification performance:"
-                        "\n\t\t>>> Accuracy: {:.1f}%"
-                        "\n\t\t>>> Precision: {:.1f}%"
-                        "\n\t\t>>> Recall: {:.1f}%"
-                        "\n\t\t>>> F1 Score: {:.1f}%".format(
-                            task_performance[0],
-                            task_performance[1],
-                            task_performance[2],
-                            task_performance[3]
-                            )
-                        )
-
-            # if there's at least two files inside current directory,
-            # run dir classification
-            # this means that also the best classified file will be computed
-            # across all files inside dir
-            else:
-                tasks_performance = []
-                tasks_train_time = []
-                tasks_test_time = []
-                tasks_best_params = []
-                tasks_tuning_times = []
-
-                # for each file inside currently considered dir
-                for input_filepath, output_filepath in zip(
-                        input_filepaths,
-                        output_filepaths
-                        ):
-                    # read input file as dataframe
-                    df = pd.read_csv(input_filepath, sep=";")
-                    if args.tune:
-                        # based on tune argument option, 
-                        # select corresponding parameters
-                        params = (param_grid if args.tune == "grid" else param_distribution)
-                        # run hyperparameter tuning
-                        (dt,
-                         tuning_best_params,
-                         tuning_time) = tune_classifier(
-                            dt, cv, params, df, args.tune
-                            )
-                        tasks_best_params.append(tuning_best_params)
-                        tasks_tuning_times.append(tuning_time)
-                    # run classification on file
-                    (splits_cm,
-                     splits_train_time,
-                     splits_test_time) = run_classification(
-                            dt, cv, df
-                            )
-                    # compute classifier results
-                    (task_performance,
-                     task_train_time,
-                     task_test_time) = compute_classifier_performance(
-                            splits_cm, splits_train_time, splits_test_time
-                            )
-                    # store classification results
-                    tasks_performance.append(task_performance)
-                    tasks_train_time.append(task_train_time)
-                    tasks_test_time.append(task_test_time)
-
-                # compute best task
-                (best_task_performance,
-                 best_task_train_time,
-                 best_task_test_time,
-                 best_task_index) = compute_best_task_performance(
-                    tasks_performance,
-                    tasks_train_time,
-                    tasks_test_time,
-                    args.metric
-                    )
-
-                # compute classification total time and per avg time per task
-                total_train_time = sum(time for time in tasks_train_time)
-                total_test_time = sum(time for time in tasks_test_time)
-                avg_train_time = np.mean([time for time in tasks_train_time])
-                avg_test_time = np.mean([time for time in tasks_test_time])
-                # compute classification information regarding tuning
-                if args.tune:
-                    best_task_params = tasks_best_params[best_task_index]
-                    total_tuning_time = sum(time for time in tasks_tuning_times)
-
-                # print classification results
-                print(
-                        "Classification results for {}:".format(
-                                input_dirpath
-                                )
-                        )
-                if args.tune:
-                    # print time taken for hyperparameter tuning
-                    print(
-                            "\tHyperparameters tuning:"
-                            "\n\t\t>>> Time taken: {:.3f}s".format(
-                                total_tuning_time
-                                )
-                            )
-                print(
-                        "\tTotal classification time (all tasks):"
-                        "\n\t\t>>> Training: {:.3f}s"
-                        "\n\t\t>>> Testing: {:.3f}s".format(
-                            total_train_time, total_test_time
-                            )
-                        )
-                print(
-                        "\tAverage classification time (per task):"
-                        "\n\t\t>>> Training: {:.3f}s"
-                        "\n\t\t>>> Testing: {:.3f}s".format(
-                            avg_train_time, avg_test_time
-                            )
-                        )
-                print(
-                        "\tBest performing task for {} was T{}:"
-                        "\n\t\tClassification time:"
-                        "\n\t\t\t>>> Training: {:.3f}s"
-                        "\n\t\t\t>>> Testing: {:.3f}s".format(
-                            input_dirpath,
-                            best_task_index + 1,
-                            best_task_train_time,
-                            best_task_test_time
-                            )
-                        )
-                print(
-                        "\t\tClassification performance:"
-                        "\n\t\t\t>>> Accuracy: {:.1f}%"
-                        "\n\t\t\t>>> Precision: {:.1f}%"
-                        "\n\t\t\t>>> Recall: {:.1f}%"
-                        "\n\t\t\t>>> F1 score: {:.1f}%".format(
-                            best_task_performance[0],
-                            best_task_performance[1],
-                            best_task_performance[2],
-                            best_task_performance[3]
-                            )
-                        )
 
 
 if __name__ == '__main__':
--- src/utils/classification.py
+++ src/utils/classification.py
@@ -1,4 +1,11 @@
+from src.utils.scan_input import scan_input_dir
+from src.utils.parameters_tuning import tune_classifier
+from src.utils.performance import compute_classifier_performance
+from src.utils.performance import compute_best_task_performance
+from src.utils.reports import print_file_classification_report
+from src.utils.reports import print_dir_classification_report
 from sklearn.metrics import confusion_matrix
+import pandas as pd
 import numpy as np
 import time
 
@@ -61,3 +68,109 @@ def run_classification(clf, cv, df):
         splits_test_time.append(testing_time)
 
     return splits_cm, splits_train_time, splits_test_time
+
+
+def file_classification(
+        clf,
+        cv,
+        input_path,
+        output_path,
+        tune_params,
+        tune_arg):
+    # read input file as dataframe
+    df = pd.read_csv(input_path, sep=";")
+    # run parameters tuning if tune argument is not None
+    if tune_arg:
+        clf, tuning_best_params, tuning_time = tune_classifier(
+                clf, cv, tune_params, df, tune_arg
+                )
+    # run classification
+    splits_cm, splits_train_time, splits_test_time = run_classification(
+            clf, cv, df
+            )
+    # compute classifier performance
+    (task_performance,
+     task_train_time,
+     task_test_time) = compute_classifier_performance(
+             splits_cm, splits_train_time, splits_test_time
+             )
+
+    return task_performance, task_train_time, task_test_time
+
+
+def dir_classification(
+        clf,
+        cv,
+        input_path,
+        output_path,
+        tune_params,
+        tune_arg,
+        metric_arg):
+    # get input file path and build corresponding output file path
+    # of all files inside input directory
+    input_paths, output_paths = scan_input_dir(input_path, output_path)
+    # for each directory found while traversing input dir
+    for input_dirpath, output_dirpath in zip(
+            sorted(input_paths),
+            sorted(output_paths)):
+        # list of paths of files inside currently considered dir
+        input_filepaths = sorted(input_paths[input_dirpath])
+        output_filepaths = sorted(output_paths[output_dirpath])
+        # if there's only one file inside the current dir,
+        if len(input_filepaths) == 1:
+            # run single file classification
+            file_classification(
+                    clf,
+                    cv,
+                    input_filepaths[0],
+                    output_filepaths[0],
+                    tune_params,
+                    tune_arg
+                    )
+        # if there's at least two files inside
+        # current directory, run dir classification
+        else:
+            # lists holding every task's classification information
+            tasks_performance = []
+            tasks_train_time = []
+            tasks_test_time = []
+            tasks_best_params = []
+            tasks_tuning_times = []
+
+            # for each file inside currently considered dir
+            for input_filepath, output_filepath in zip(
+                    input_filepaths,
+                    output_filepaths
+                    ):
+                # run classification process on file
+                (task_performance,
+                 task_train_time,
+                 task_test_time) = file_classification(
+                         clf,
+                         cv,
+                         input_filepath,
+                         output_filepath,
+                         tune_params,
+                         tune_arg
+                         )
+                # store classification results
+                tasks_performance.append(task_performance)
+                tasks_train_time.append(task_train_time)
+                tasks_test_time.append(task_test_time)
+
+            # compute best task
+            (best_task_performance,
+             best_task_train_time,
+             best_task_test_time,
+             best_task_index) = compute_best_task_performance(
+                tasks_performance,
+                tasks_train_time,
+                tasks_test_time,
+                metric_arg
+                )
+
+            # compute classification total time and per avg time per task
+            total_train_time = sum(time for time in tasks_train_time)
+            total_test_time = sum(time for time in tasks_test_time)
+            avg_train_time = np.mean([time for time in tasks_train_time])
+            avg_test_time = np.mean([time for time in tasks_test_time])
