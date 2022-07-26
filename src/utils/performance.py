06e6c7201465bf59af3b53e56e5f3c4a0ec8befb
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-26 18:23:37 +0200

    add function to compute entire classification report

--- src/utils/performance.py
+++ src/utils/performance.py
@@ -1,70 +1,42 @@
-def compute_classifier_performance(
-        split_cm,
-        split_train_time,
-        split_test_time
-        ):
-    # compute task classifier performance from split results
-    task_cm = sum(cm for cm in split_cm)
-    task_train_time = sum(time for time in split_train_time)
-    task_test_time = sum(time for time in split_test_time)
-
-    # get each matrix cell separately
-    tn, fp, fn, tp = task_cm.ravel()
-
-    # compute task matrix performance
-    accuracy = ((tp + tn) / (tn + fp + fn + tp) * 100)
-    precision = (tp / (tp + fp) * 100)
-    recall = (tp / (tp + fn) * 100)
-    f1_score = (2 * precision * recall / (precision + recall))
-
-    return (
-            (accuracy, precision, recall, f1_score),
-            task_train_time,
-            task_test_time
-            )
+import numpy as np
 
 
-def compute_best_task_performance(
-        tasks_performance,
-        tasks_train_time,
-        tasks_test_time,
-        p_metric
-        ):
-    # map metric argument to corresponging index in performance tuple
-    metric = {'accuracy': 0, 'precision': 1, 'recall': 2, 'f1': 3}[p_metric]
+def compute_classification_report(run_results_dict):
+    # Dictionary containing classification results for each run
+    classification_report = {}
+    # For each performed run
+    for run in run_results_dict:
+        # Lists holding classification performance metrics
+        accuracies = []
+        precisions = []
+        recalls = []
+        # Lists holding classification times
+        train_times = []
+        test_times = []
 
-    # Compute best task classification information
-    best_task_metric_value = max(
-            [
-                results[metric]
-                for results
-                in tasks_performance
-                ]
-            )
-    best_task_index = (
-            [
-                results[metric]
-                for results
-                in tasks_performance
-                ]
-            .index(best_task_metric_value)
-            )
-    best_task_train_time = tasks_train_time[best_task_index]
-    best_task_test_time = tasks_test_time[best_task_index]
+        for confusion_matrix, times in zip(run_results_dict[run][0],
+                                           run_results_dict[run][1]):
+            # Ravel currently evaluated confusion matrix
+            tn, fp, fn, tp = confusion_matrix.ravel()
+            # Compute and append performance metrics to corresponding lists
+            accuracies.append((tp + tn) / (tn + fp + fn + tp) * 100)
+            precisions.append((tp / (tp + fp) * 100))
+            recalls.append((tp / (tp + fn) * 100))
+            # Append classification times to corresponding lists
+            train_times.append(times[0])
+            test_times.append(times[1])
 
-    # Obtain best task performances
-    best_task_accuracy = tasks_performance[best_task_index][0]
-    best_task_precision = tasks_performance[best_task_index][1]
-    best_task_recall = tasks_performance[best_task_index][2]
-    best_task_f1score = tasks_performance[best_task_index][3]
+        # Compute mean and standard deviation
+        # for all metric arrays
+        run_performance = ((np.mean(accuracies), np.std(accuracies)),
+                           (np.mean(precisions), np.std(precisions)),
+                           (np.mean(recalls), np.std(recalls)))
+        # Compute total training and testing times
+        run_times = (sum(time for time in train_times),
+                     sum(time for time in test_times))
+        # Append currently evaluated run results
+        # to corresponding entry in classification report
+        classification_report[run] = (run_performance,
+                                      run_times)
 
-    return (
-            (best_task_accuracy,
-             best_task_precision,
-             best_task_recall,
-             best_task_f1score
-             ),
-            best_task_train_time,
-            best_task_test_time,
-            best_task_index
-            )
+    return classification_report
