a58f3c362f6c32ed80b40bea1613877deb30311a
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-26 19:23:24 +0200

    add model total stddev for each performance metric

--- src/utils/performance.py
+++ src/utils/performance.py
@@ -26,11 +26,11 @@ def compute_classification_report(run_results_dict):
             train_times.append(times[0])
             test_times.append(times[1])
 
-        # Compute mean and standard deviation
+        # Compute mean and variance
         # for all metric arrays
-        run_performance = ((np.mean(accuracies), np.std(accuracies)),
-                           (np.mean(precisions), np.std(precisions)),
-                           (np.mean(recalls), np.std(recalls)))
+        run_performance = ((np.mean(accuracies), np.var(accuracies)),
+                           (np.mean(precisions), np.var(precisions)),
+                           (np.mean(recalls), np.var(recalls)))
         # Compute total training and testing times
         run_times = (sum(time for time in train_times),
                      sum(time for time in test_times))
--- src/utils/reports.py
+++ src/utils/reports.py
@@ -1,24 +1,59 @@
-def print_classification_report(classification_report):
-    print(classification_report)
+import numpy as np
+
+
+def print_classification_report(input, classification_report):
+    accuracy_mean_list = []
+    accuracy_variance_list = []
+    precision_mean_list = []
+    precision_variance_list = []
+    recall_mean_list = []
+    recall_variance_list = []
+    print("CLASSIFICATION REPORT FOR '{}'". format(input))
     for run in classification_report:
-        print("Run n.{}:".format(run))
-        print("\t>>> Accuracy:"
-              "\n\t\t>>> mean: {:.1f}%"
-              "\n\t\t>>> stdev: {:.1f}%"
-              .format(classification_report[run][0][0][0],
-                      classification_report[run][0][0][1]))
-        print("\t>>> Precision:"
-              "\n\t\t>>> mean: {:.1f}%"
-              "\n\t\t>>> stdev: {:.1f}%"
-              .format(classification_report[run][0][1][0],
-                      classification_report[run][0][1][1]))
-        print("\t>>> Recall:"
-              "\n\t\t>>> mean: {:.1f}%"
-              "\n\t\t>>> stdev: {:.1f}%"
-              .format(classification_report[run][0][2][0],
-                      classification_report[run][0][2][1]))
-        print("\t>>> Run times:"
-              "\n\t\t>>> Total training time: {:.3f}s"
-              "\n\t\t>>> Total testing time: {:.3f}s"
-              .format(classification_report[run][1][0],
-                      classification_report[run][1][1]))
+        acc_mean = classification_report[run][0][0][0]
+        acc_var = classification_report[run][0][0][1]
+        prec_mean = classification_report[run][0][1][0]
+        prec_var = classification_report[run][0][1][1]
+        rec_mean = classification_report[run][0][2][0]
+        rec_var = classification_report[run][0][2][1]
+        train_time = classification_report[run][1][0]
+        test_time = classification_report[run][1][1]
+        print("Run [{}]:".format(run))
+        print("\tAccuracy:"
+              "\tmean={:.1f}%"
+              "\tstdev={:.1f}%"
+              .format(acc_mean, np.sqrt(acc_var)))
+        print("\tPrecision:"
+              "\tmean={:.1f}%"
+              "\tstdev={:.1f}%"
+              .format(prec_mean, np.sqrt(prec_var)))
+        print("\tRecall:"
+              "\t\tmean={:.1f}%"
+              "\tstdev={:.1f}%"
+              .format(rec_mean, np.sqrt(rec_var)))
+        print("\tTimes:"
+              "\t\ttraining={:.3f}s"
+              "\ttesting={:.3f}s"
+              .format(train_time, test_time))
+        accuracy_mean_list.append(acc_mean)
+        precision_mean_list.append(prec_mean)
+        recall_mean_list.append(rec_mean)
+        accuracy_variance_list.append(acc_var)
+        precision_variance_list.append(prec_var)
+        recall_variance_list.append(rec_var)
+    print("")
+    print("Model accuracy:"
+          "\t\tmean={:.1f}%"
+          "\tstdev={:.1f}%"
+          .format(np.mean(accuracy_mean_list),
+                  np.sqrt(np.mean(accuracy_variance_list))))
+    print("Model precision:"
+          "\tmean={:.1f}%"
+          "\tstdev={:.1f}%"
+          .format(np.mean(precision_mean_list),
+                  np.sqrt(np.mean(precision_variance_list))))
+    print("Model recall:"
+          "\t\tmean={:.1f}%"
+          "\tstdev={:.1f}%"
+          .format(np.mean(recall_mean_list),
+                  np.sqrt(np.mean(recall_variance_list))))
