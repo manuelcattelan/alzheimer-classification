4c4b734a9357681955040ba72e42daf3b4547039
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-27 09:43:28 +0200

    plot classification report instead of printing to terminal

--- src/utils/reports.py
+++ src/utils/reports.py
@@ -1,23 +1,18 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
-def print_classification_report(input, classification_report):
-    accuracy_mean_list = []
-    accuracy_variance_list = []
-    precision_mean_list = []
-    precision_variance_list = []
-    recall_mean_list = []
-    recall_variance_list = []
-    print("CLASSIFICATION REPORT FOR '{}'". format(input))
-    for run in classification_report:
-        acc_mean = classification_report[run][0][0][0]
-        acc_var = classification_report[run][0][0][1]
-        prec_mean = classification_report[run][0][1][0]
-        prec_var = classification_report[run][0][1][1]
-        rec_mean = classification_report[run][0][2][0]
-        rec_var = classification_report[run][0][2][1]
-        train_time = classification_report[run][1][0]
-        test_time = classification_report[run][1][1]
+def print_runs_report(input, runs_report):
+    print("RUNS REPORT FOR '{}'". format(input))
+    for run in runs_report:
+        acc_mean = runs_report[run][0][0][0]
+        acc_var = runs_report[run][0][0][1]
+        prec_mean = runs_report[run][0][1][0]
+        prec_var = runs_report[run][0][1][1]
+        rec_mean = runs_report[run][0][2][0]
+        rec_var = runs_report[run][0][2][1]
+        train_time = runs_report[run][1][0]
+        test_time = runs_report[run][1][1]
         print("Run [{}]:".format(run))
         print("\tAccuracy:"
               "\tmean={:.1f}%"
@@ -35,25 +30,36 @@ def print_classification_report(input, classification_report):
               "\t\ttraining={:.3f}s"
               "\ttesting={:.3f}s"
               .format(train_time, test_time))
-        accuracy_mean_list.append(acc_mean)
-        precision_mean_list.append(prec_mean)
-        recall_mean_list.append(rec_mean)
-        accuracy_variance_list.append(acc_var)
-        precision_variance_list.append(prec_var)
-        recall_variance_list.append(rec_var)
-    print("")
-    print("Model accuracy:"
-          "\t\tmean={:.1f}%"
-          "\tstdev={:.1f}%"
-          .format(np.mean(accuracy_mean_list),
-                  np.sqrt(np.mean(accuracy_variance_list))))
-    print("Model precision:"
-          "\tmean={:.1f}%"
-          "\tstdev={:.1f}%"
-          .format(np.mean(precision_mean_list),
-                  np.sqrt(np.mean(precision_variance_list))))
-    print("Model recall:"
-          "\t\tmean={:.1f}%"
-          "\tstdev={:.1f}%"
-          .format(np.mean(recall_mean_list),
-                  np.sqrt(np.mean(recall_variance_list))))
+
+def plot_classification_report(input, classification_report):
+    metrics = ["Accuracy", "Precision", "Recall"]
+
+    accuracy_mean = classification_report[0][0]
+    accuracy_stdev = classification_report[0][1]
+    precision_mean = classification_report[1][0]
+    precision_stdev = classification_report[1][1]
+    recall_mean = classification_report[2][0]
+    recall_stdev = classification_report[2][1]
+
+    CTEs = [accuracy_mean, precision_mean, recall_mean]
+    error = [accuracy_stdev, precision_stdev, recall_stdev]
+
+    x_pos = np.arange(len(metrics))
+    y_pos = np.arange(0, 100 + 10, 10)
+
+    fig, ax = plt.subplots()
+    ax.bar(x_pos,
+           CTEs,
+           yerr=error,
+           align='center',
+           alpha=0.5,
+           ecolor='black',
+           capsize = 10)
+    ax.set_ylabel("Classification result")
+    ax.set_xticks(x_pos)
+    ax.set_yticks(y_pos)
+    ax.set_xticklabels(metrics)
+    ax.set_title("Classification report for " + input)
+
+    plt.tight_layout()
+    plt.show()
