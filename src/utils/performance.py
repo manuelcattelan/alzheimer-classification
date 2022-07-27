5c0abc3f6904e16bdcc9668f8fc7c9cf38aec89c
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-27 09:44:02 +0200

    separate compute run and compute classification report

--- src/utils/performance.py
+++ src/utils/performance.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 
-def compute_classification_report(run_results_dict):
+def compute_runs_report(run_results_dict):
     # Dictionary containing classification results for each run
-    classification_report = {}
+    runs_report = {}
     # For each performed run
     for run in run_results_dict:
         # Lists holding classification performance metrics
@@ -36,7 +36,42 @@ def compute_classification_report(run_results_dict):
                      sum(time for time in test_times))
         # Append currently evaluated run results
         # to corresponding entry in classification report
-        classification_report[run] = (run_performance,
-                                      run_times)
+        runs_report[run] = (run_performance,run_times)
 
-    return classification_report
+    return runs_report
+
+
+def compute_classification_report(runs_report):
+    accuracy_mean_list = []
+    accuracy_variance_list = []
+    precision_mean_list = []
+    precision_variance_list = []
+    recall_mean_list = []
+    recall_variance_list = []
+    for run in runs_report:
+        acc_mean = runs_report[run][0][0][0]
+        acc_var = runs_report[run][0][0][1]
+        prec_mean = runs_report[run][0][1][0]
+        prec_var = runs_report[run][0][1][1]
+        rec_mean = runs_report[run][0][2][0]
+        rec_var = runs_report[run][0][2][1]
+        train_time = runs_report[run][1][0]
+        test_time = runs_report[run][1][1]
+
+        accuracy_mean_list.append(acc_mean)
+        precision_mean_list.append(prec_mean)
+        recall_mean_list.append(rec_mean)
+        accuracy_variance_list.append(acc_var)
+        precision_variance_list.append(prec_var)
+        recall_variance_list.append(rec_var)
+
+    accuracy_mean = np.mean(accuracy_mean_list)
+    accuracy_stdev = np.sqrt(np.mean(accuracy_variance_list))
+    precision_mean = np.mean(precision_mean_list)
+    precision_stdev = np.sqrt(np.mean(precision_variance_list))
+    recall_mean = np.mean(recall_mean_list)
+    recall_stdev = np.sqrt(np.mean(recall_variance_list))
+
+    return ((accuracy_mean, accuracy_stdev),
+            (precision_mean, precision_stdev),
+            (recall_mean, recall_stdev))
