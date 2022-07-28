1a3c64dcdad3af25656d68f04e225eb1b215a119
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-28 15:01:29 +0200

    add parameter distributions for models

--- src/utils/parameters_tuning.py
+++ src/utils/parameters_tuning.py
@@ -1,31 +1,56 @@
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
+from scipy.stats import randint
+from scipy.stats import loguniform
 import numpy as np
 import time
 
 
 dt_parameters = {"criterion": ["gini", "entropy"],
                  "splitter": ["best", "random"],
-                 "max_depth": [None, 1, 3, 5, 8, 10],
-                 "min_samples_split": [2, 5, 10, 15, 20],
-                 "min_samples_leaf": [1, 5, 10, 15, 20],
+                 "max_depth": [None, 1, 2, 3, 4, 5],
+                 "min_samples_split": [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+                 "min_samples_leaf": [1, 3, 5, 8, 10, 12, 14, 16, 18, 20],
                  "min_impurity_decrease": [0.1, 0.2, 0.3, 0.4, 0.5]}
 
 
-rf_parameters = {"n_estimators": [50, 100, 150, 200, 250],
+dt_distribution = {"criterion": ["gini", "entropy"],
+                   "splitter": ["best", "random"],
+                   "max_depth": [None, 1, 2, 3, 4, 5],
+                   "min_samples_split": randint(low=2, high=20),
+                   "min_samples_leaf": randint(low=1, high=20),
+                   "min_impurity_decrease": [0.1, 0.2, 0.3, 0.4, 0.5]}
+
+
+rf_parameters = {"n_estimators": [10, 100, 200, 300, 400, 500],
                  "criterion": ["gini", "entropy"],
-                 "max_depth": [None, 1, 3, 5, 8, 10],
-                 "min_samples_split": [2, 5, 10, 15, 20],
-                 "min_samples_leaf": [1, 5, 10, 15, 20],
+                 "max_depth": [None, 1, 2, 3, 4, 5],
+                 "min_samples_split": [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+                 "min_samples_leaf": [1, 3, 5, 8, 10, 12, 14, 16, 18, 20],
                  "min_impurity_decrease": [0.1, 0.2, 0.3, 0.4, 0.5],
                  "bootstrap": [True, False]}
 
 
-svc_parameters = {"C": [0.1, 1, 10, 100, 1000],
+rf_distribution = {"n_estimators": randint(10, 250),
+                   "criterion": ["gini", "entropy"],
+                   "max_depth": [None, 1, 2, 3, 4, 5],
+                   "min_samples_split": randint(2, 20),
+                   "min_samples_leaf": randint(2, 20),
+                   "min_impurity_decrease": [0.1, 0.2, 0.3, 0.4, 0.5],
+                   "bootstrap": [True, False]}
+
+
+svc_parameters = {"C": [1, 10, 100, 1000],
                   "kernel": ["linear", "poly", "rbf"],
                   "degree": [1, 2, 3, 4, 5],
                   "gamma": ["scale", "auto", 0.0001, 0.001, 0.1, 1, 10, 100]}
+
+
+svc_distribution = {"C": loguniform(1e0, 1e3),
+                    "kernel": ["linear", "poly", "rbf"],
+                    "degree": randint(1, 5),
+                    "gamma": loguniform(1e-4, 1e2)}
 
 
 def tune_classifier(clf,
@@ -50,13 +75,16 @@ def tune_classifier(clf,
                                        n_iter=tune_iter,
                                        scoring=tune_metric,
                                        n_jobs=n_jobs,
-                                       cv=StratifiedKFold(shuffle=True,))
+                                       cv=StratifiedKFold(shuffle=True,
+                                                          random_state=0),
+                                       random_state=0)
         case "grid":
             tuner = GridSearchCV(estimator=clf,
                                  param_grid=tune_parameters,
                                  scoring=tune_metric,
                                  n_jobs=n_jobs,
-                                 cv=StratifiedKFold(shuffle=True,))
+                                 cv=StratifiedKFold(shuffle=True,
+                                                    random_state=0))
     # Tune parameters on dataframe
     start = time.time()
     tuner.fit(X, y)
