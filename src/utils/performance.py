2537eb6b73436aad992e5d9008571ddcb5b6308a
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-29 16:06:15 +0200

    add function to export results in csv file

--- src/utils/performance.py
+++ src/utils/performance.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 import numpy as np
+import csv
+import os
 
 
 def compute_clf_results(raw_results):
@@ -7,7 +10,7 @@ def compute_clf_results(raw_results):
     for run_no in raw_results:
         # Compute mean over all splits for each performance metric
         run_performance_mean = [
-                sum(metric)/len(raw_results[run_no][0]) 
+                sum(metric)/len(raw_results[run_no][0])
                 for metric in zip(*raw_results[run_no][0])
                 ]
         # Compute variance over all splits for each performance metric
@@ -21,7 +24,50 @@ def compute_clf_results(raw_results):
                 ]
         # Add results to [run_no] in dictionary
         clf_results[run_no] = (
-                (run_performance_mean, run_performance_var),
+                run_performance_mean,
+                run_performance_var,
                 run_runtime
                 )
     return clf_results
+
+
+def export_clf_results(clf_results, clf_best_params, tune_time, output_path):
+    # Get directory name from output path
+    output_dir = Path(os.path.dirname(output_path))
+    # Create output directory if it does not exist
+    output_dir.mkdir(parents=True, exist_ok=True)
+    # Add csv extension to output path
+    output_with_no_suffix = Path(output_path).with_suffix("")
+    output_with_csv_suffix = Path(output_with_no_suffix).with_suffix(".csv")
+
+    with open(output_with_csv_suffix, "w+", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        header = [
+                "run_no",
+                "parameters",
+                "acc_mean[%]",
+                "acc_stdev[%]",
+                "prec_mean[%]",
+                "prec_stdev[%]",
+                "rec_mean[%]",
+                "rec_stdev[%]",
+                "train_time[s]",
+                "test_time[s]",
+                "tune_time[s]"
+                ]
+        writer.writerow(header)
+        for run_no in clf_results:
+            data = [
+                    run_no,
+                    clf_best_params if tune_time is not None else "default",
+                    "{:%}".format(clf_results[run_no][0][0]),
+                    "{:%}".format(np.sqrt(clf_results[run_no][1][0])),
+                    "{:%}".format(clf_results[run_no][0][1]),
+                    "{:%}".format(np.sqrt(clf_results[run_no][1][1])),
+                    "{:%}".format(clf_results[run_no][0][2]),
+                    "{:%}".format(np.sqrt(clf_results[run_no][1][2])),
+                    "{:.4f}".format(clf_results[run_no][2][0]),
+                    "{:.4f}".format(clf_results[run_no][2][1]),
+                    "{:.4f}".format(tune_time) if tune_time is not None else 0,
+                    ]
+            writer.writerow(data)
