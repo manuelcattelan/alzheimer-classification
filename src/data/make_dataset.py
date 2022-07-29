338c40878ce8b2b4ee3db5cb2d7d70400dc6e17b
manuelcattelan <cattelanmanuel07@gmail.com>
2022-07-29 10:27:30 +0200

    refactor and cleanup code

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,60 +1,89 @@
-from src.utils.preprocessing import file_preprocessing
-from src.utils.preprocessing import dir_preprocessing
+from src.utils.path import build_path
+from src.utils.data_preprocessing import preprocess_dataframe
+from src.utils.data_preprocessing import export_dataframe
+import pandas as pd
 import argparse
 import errno
 import os
 
 
 def main():
-    # Set un parser to enable possible arguments from command line
+    # Setup parser to enable command line arguments
     parser = argparse.ArgumentParser(
-            formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("--input",
-                        help=("path to file or directory of files where "
-                              "data to preprocess is stored"),
-                        required=True)
-    parser.add_argument("--output",
-                        help=("path to file or directory of files where "
-                              "preprocessed data will be stored"),
-                        required=True)
+            formatter_class=argparse.RawDescriptionHelpFormatter
+            )
+    parser.add_argument(
+            "--input",
+            help=("path to file or directory of files where data "
+                  "to preprocess is stored"),
+            required=True
+            )
+    parser.add_argument(
+            "--output",
+            help=("path to file or directory of files where "
+                  "preprocessed data will be stored"),
+            required=True
+            )
     args = parser.parse_args()
 
     # Check if provided input argument is valid, meaning:
-    # Input argument is an existing file, or
-    # Input argument is an existing directory
+    # args.input is a path to an existing file, or
+    # args.input is a path to an existing directory
     if not os.path.exists(args.input):
-        raise FileNotFoundError(errno.ENOENT,
-                                os.strerror(errno.ENOENT),
-                                args.input)
+        raise FileExistsError(
+                errno.ENOENT,
+                os.strerror(errno.ENOENT),
+                args.input
+                )
 
     # Get extension from output argument for later validity checks:
-    # If input is a file -> output must end with .csv extension
-    # If input is a directory -> output must end with no extension
-    output_arg_extension = os.path.splitext(args.output)[1]
+    # If args.input is a file -> args.output must end with .csv extension
+    # If args.input is a directory -> args.output must end with no extension
+    output_extension = os.path.splitext(args.output)[1]
 
-    # Check if provided input argument contains path to file
+    # Check if provided input argument holds path to existing file
     if os.path.isfile(args.input):
-        if output_arg_extension != ".csv":
-            raise ValueError("Not a valid path to csv file: '"
-                             + args.output
-                             + "'")
-        # If everything is OK:
-        # run preprocessing on specified input file
-        # store preprocessed data on specified output file
-        file_preprocessing(input_path=args.input,
-                           output_path=args.output)
+        if output_extension != ".csv":
+            raise ValueError(
+                    "not a valid path to csv file: '"
+                    + args.output
+                    + "'"
+                    )
+        # If all conditions are met:
+        # read raw data
+        # preprocess raw data
+        # export preprocessed data
+        df_to_preprocess = pd.read_csv(args.input, sep=";")
+        df_preprocessed = preprocess_dataframe(df_to_preprocess)
+        export_dataframe(df_preprocessed, args.output)
 
-    # Check if provided input argument contains path to directory
+    # Check if provided input argument holds path to existing directory
     if os.path.isdir(args.input):
-        if output_arg_extension != "":
-            raise ValueError("Not a valid path to directory: '"
-                             + args.output
-                             + "'")
-        # If everything is OK:
-        # run preprocessing on specified input directory
-        # store preprocessed data on specified output directory
-        dir_preprocessing(input_path=args.input,
-                          output_path=args.output)
+        if output_extension != "":
+            raise ValueError(
+                    "not a valid path to directory: '"
+                    + args.output
+                    + "'"
+                    )
+        # If all conditions are met:
+        # Build input/output paths for each file/directory inside args.input
+        input_paths, output_paths = build_path(args.input, args.output)
+        # For each directory inside args.input
+        for input_dirpath, output_dirpath in zip(
+                sorted(input_paths),
+                sorted(output_paths)
+                ):
+            # For each file inside directory
+            for input_filepath, output_filepath in zip(
+                    sorted(input_paths[input_dirpath]),
+                    sorted(output_paths[output_dirpath])
+                    ):
+                # read raw data
+                # preprocess raw data
+                # export preprocessed data
+                df_to_preprocess = pd.read_csv(input_filepath, sep=";")
+                df_preprocessed = preprocess_dataframe(df_to_preprocess)
+                export_dataframe(df_preprocessed, output_filepath)
 
 
 if __name__ == "__main__":
--- src/utils/data_preprocessing.py
+++ src/utils/data_preprocessing.py
@@ -1,21 +1,29 @@
-from src.utils.path import build_path
 from sklearn.preprocessing import MinMaxScaler
-from scipy import stats
 from pathlib import Path
-import pandas as pd
+from scipy import stats
 import numpy as np
 import os
 
 
-def map_data(df_to_map):
+def strip_dataframe(df_to_strip):
+    df_stripped = df_to_strip.copy()
+
+    # For each feature in features, strip feature
+    for feature in ["Sex", "Work", "Label"]:
+        df_stripped[feature] = df_to_strip[feature].str.strip()
+
+    return df_stripped
+
+
+def map_dataframe(df_to_map):
     df_mapped = df_to_map.copy()
 
-    # Mapping dictionaries for alphabetical features
+    # List of dictionaries that map features from alphabetic to numeric
     sex_map = {"Maschile": 0, "Femminile": 1}
     work_map = {"Manuale": 0, "Intellettuale": 1}
     label_map = {"Sano": 0, "Malato": 1}
 
-    # Map alphabetical features to numeric features
+    # Perform mapping on all alphabetic features
     df_mapped["Sex"] = df_to_map["Sex"].map(sex_map)
     df_mapped["Work"] = df_to_map["Work"].map(work_map)
     df_mapped["Label"] = df_to_map["Label"].map(label_map)
@@ -23,102 +31,70 @@ def map_data(df_to_map):
     return df_mapped
 
 
-def clean_data(df_to_clean):
+def clean_dataframe(df_to_clean):
     df_cleaned = df_to_clean.copy()
 
-    # Define range of features that need to be cleaned:
-    # adjacent feature columns indicate features that are contiguous
-    adjacent_feature_columns = ["DurationTot", "AveragePenPressureVar"]
-    # Define numeric range for features that need to be cleaned
-    features_to_clean = np.r_[
-            df_to_clean.columns.get_loc(adjacent_feature_columns[0]):
-            df_to_clean.columns.get_loc(adjacent_feature_columns[1]) + 1]
-    # Create boolean mask for all rows that
-    # only contain 0 in the defined feature range
-    rows_to_clean_mask = df_to_clean.iloc[:, features_to_clean].eq(0).all(1)
-    # Get a list of indexes for rows that match mask
-    rows_to_clean_index = list(df_to_clean[rows_to_clean_mask].index.values)
+    # Get range of features that need to be checked and cleaned
+    features = ["DurationTot", "AveragePenPressureVar"]
+    features_range = np.r_[
+            df_to_clean.columns.get_loc(features[0]):
+            df_to_clean.columns.get_loc(features[1]) + 1
+            ]
 
-    # Drop rows that match mask from dataframe
+    # Get list of indexes for all rows that only contain 0's in defined range
+    rows_to_clean = df_to_clean.iloc[:, features_range].eq(0).all(1)
+    rows_to_clean_index = list(df_to_clean[rows_to_clean].index.values)
+
+    # Drop rows that only contain 0's in defined range
     df_cleaned = df_to_clean.drop(rows_to_clean_index)
-    # Drop columns that only contain 0's
+    # Drop columnss that only contain 0's
     df_cleaned = df_cleaned.loc[:, (df_cleaned != 0).any(axis=0)]
     # Drop rows that have outliers in at least one column
     df_cleaned = df_cleaned[(np.abs(stats.zscore(df_cleaned)) < 3).all(axis=1)]
-    # Drop columns which only contain 0's after removing outliers
+    # Drop columns that only contain 0's after removing outliers
     df_cleaned = df_cleaned.loc[:, (df_cleaned != 0).any(axis=0)]
 
     return df_cleaned
 
 
-def normalize_data(df_to_normalize):
+def normalize_dataframe(df_to_normalize):
     df_normalized = df_to_normalize.copy()
 
-    # Define range of features that need to be normalized:
-    # non adjacent feature columns indicate features that are not contiguous
-    # adjacent feature columns indicate features that are contiguous
-    non_adjacent_feature_columns = ["Age", "Instruction"]
-    adjacent_feature_columns = ["DurationTot", "NumOfStrokes"]
-    # Define numeric range for features that need to be normalized
-    features_to_normalize = np.r_[
-            df_to_normalize.columns.get_indexer(non_adjacent_feature_columns),
-            df_to_normalize.columns.get_loc(adjacent_feature_columns[0]):
-            df_to_normalize.columns.get_loc(adjacent_feature_columns[1]) + 1]
-    # Define names for features that need to be normalized
-    features_to_normalize_name = df_to_normalize.columns[features_to_normalize]
+    # Get range of features that need to be normalized
+    contiguous_features = ["DurationTot", "NumOfStrokes"]
+    non_contiguous_features = ["Age", "Instruction"]
+    features_range = np.r_[
+            df_to_normalize.columns.get_indexer(non_contiguous_features),
+            df_to_normalize.columns.get_loc(contiguous_features[0]):
+            df_to_normalize.columns.get_loc(contiguous_features[1]) + 1
+            ]
+    features_names = df_to_normalize.columns[features_range]
 
     # Initialize and apply MinMaxScaler to dataframe
     scaler = MinMaxScaler()
-    df_normalized[features_to_normalize_name] = scaler.fit_transform(
-            df_to_normalize[features_to_normalize_name])
+    df_normalized[features_names] = scaler.fit_transform(
+            df_to_normalize[features_names]
+            )
 
     return df_normalized
 
 
-def export_data(df_to_export, output_path):
-    # Get directory name from output path
-    output_dirname = Path(os.path.dirname(output_path))
-    # Create output directory if it does not exist
-    output_dirname.mkdir(parents=True, exist_ok=True)
-    # Export dataframe to output directory
-    df_to_export.to_csv(output_path, sep=";", index=False)
-
-
-def run_preprocessing(df_raw):
-    # Run data preprocessing steps
-    df_mapped = map_data(df_to_map=df_raw)
-    df_cleaned = clean_data(df_to_clean=df_mapped)
+def preprocess_dataframe(df_raw):
+    # List of preprocessing steps:
+    # Strip dataframe
+    # Map dataframe
+    # Clean dataframe
+    df_stripped = strip_dataframe(df_raw)
+    df_mapped = map_dataframe(df_stripped)
+    df_cleaned = clean_dataframe(df_mapped)
 
     return df_cleaned
 
 
-def file_preprocessing(input_path, output_path):
-    # Read raw data from input path into dataframe
-    df_to_preprocess = pd.read_csv(input_path,
-                                   sep=";",
-                                   converters={"Sex": str.strip,
-                                               "Work": str.strip,
-                                               "Label": str.strip})
-    # Preprocess raw dataframe
-    df_preprocessed = run_preprocessing(df_raw=df_to_preprocess)
-    # Export preprocessed dataframe to output path
-    export_data(df_to_export=df_preprocessed,
-                output_path=output_path)
-
-
-def dir_preprocessing(input_path, output_path):
-    # Recursively scan input path in order to:
-    # build a list of all input paths to read
-    # build a list of all corresponding output paths to write
-    input_path_list, output_path_list = build_path(input_path, output_path)
-    # For each directory path found while traversing input path
-    for input_dirpath, output_dirpath in zip(sorted(input_path_list),
-                                             sorted(output_path_list)):
-        # For each file path found inside currently considered directory
-        for input_filepath, output_filepath in zip(
-                sorted(input_path_list[input_dirpath]),
-                sorted(output_path_list[output_dirpath])):
-            # Run preprocessing on specified input file
-            # Store preprocessed data on specified output file
-            file_preprocessing(input_path=input_filepath,
-                               output_path=output_filepath)
+def export_dataframe(df_to_export, output_path):
+    # Get directory name from output path
+    output_dir = Path(os.path.dirname(output_path))
+    # Create output directory if it does not exist
+    output_dir.mkdir(parents=True, exist_ok=True)
+    # Export dataframe to output path
+    df_to_export.to_csv(output_path, sep=";", index=False)
