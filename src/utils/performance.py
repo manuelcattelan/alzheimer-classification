31d49dee5fd93a40b28c4ab2e6ce60f535bd899f
manuelcattelan <cattelanmanuel07@gmail.com>
2022-08-01 09:38:36 +0200

    export classification summary and change csv format

--- src/utils/performance.py
+++ src/utils/performance.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import os
@@ -31,7 +32,7 @@ def compute_clf_results(raw_results):
     return clf_results
 
 
-def export_clf_results(clf_results, clf_best_params, tune_time, output_path):
+def export_clf_report(clf_results, output_path):
     # Get directory name from output path
     output_dir = Path(os.path.dirname(output_path))
     # Create output directory if it does not exist
@@ -45,17 +46,12 @@ def export_clf_results(clf_results, clf_best_params, tune_time, output_path):
         writer = csv.writer(csvfile)
         # Define header row
         header = [
-                "run_no",
-                "parameters",
-                "splits_accuracy_mean (%)",
-                "splits_accuracy_stdev (%)",
-                "splits_precision_mean (%)",
-                "splits_precision_stdev (%)",
-                "splits_recall_mean (%)",
-                "splits_recall_stdev (%)",
-                "splits_train_time (s)",
-                "splits_test_time (s)",
-                "splits_tune_time (s)"
+                "Run",
+                "Accuracy [%]",
+                "Precision [%]",
+                "Recall [%]",
+                "Train time [s]",
+                "Test time [s]",
                 ]
         # Write header row
         writer.writerow(header)
@@ -65,67 +61,108 @@ def export_clf_results(clf_results, clf_best_params, tune_time, output_path):
             # Define data row
             data = [
                     run_no,
-                    clf_best_params if tune_time is not None else "default",
-                    "{:%}".format(clf_results[run_no][0][0]),
-                    "{:%}".format(np.sqrt(clf_results[run_no][1][0])),
-                    "{:%}".format(clf_results[run_no][0][1]),
-                    "{:%}".format(np.sqrt(clf_results[run_no][1][1])),
-                    "{:%}".format(clf_results[run_no][0][2]),
-                    "{:%}".format(np.sqrt(clf_results[run_no][1][2])),
+                    "{:.2f} (±{:.2f})".format(
+                        clf_results[run_no][0][0] * 100,
+                        clf_results[run_no][1][0] * 100
+                        ),
+                    "{:.2f} (±{:.2f})".format(
+                        clf_results[run_no][0][1] * 100,
+                        clf_results[run_no][1][1] * 100
+                        ),
+                    "{:.2f} (±{:.2f})".format(
+                        clf_results[run_no][0][2] * 100,
+                        clf_results[run_no][1][2] * 100
+                        ),
                     "{:.4f}".format(clf_results[run_no][2][0]),
                     "{:.4f}".format(clf_results[run_no][2][1]),
-                    "{:.4f}".format(tune_time) if tune_time is not None else 0,
                     ]
             # Write data row
             writer.writerow(data)
 
-        # Compute average classification results from row results
-        runs_accuracy_mean = sum(
-                means[0][0]
-                for means in clf_results.values()
+
+def export_clf_summary(clf_results, output_path):
+    # Compute averaged results for classification
+    runs_accuracy_mean = sum(
+            means[0][0]
+            for means in clf_results.values()
+            ) / float(len(clf_results)) * 100
+    runs_accuracy_stdev = np.sqrt(
+            sum(vars[1][0]
+                for vars in clf_results.values()
                 ) / float(len(clf_results))
-        runs_accuracy_stdev = np.sqrt(
-                sum(vars[1][0]
-                    for vars in clf_results.values()
-                    ) / float(len(clf_results))
-                )
-        runs_precision_mean = sum(
-                means[0][1]
-                for means in clf_results.values()
+            ) * 100
+    runs_precision_mean = sum(
+            means[0][1]
+            for means in clf_results.values()
+            ) / float(len(clf_results)) * 100
+    runs_precision_stdev = np.sqrt(
+            sum(vars[1][1]
+                for vars in clf_results.values()
                 ) / float(len(clf_results))
-        runs_precision_stdev = np.sqrt(
-                sum(vars[1][1]
-                    for vars in clf_results.values()
-                    ) / float(len(clf_results))
-                )
-        runs_recall_mean = sum(
-                means[0][2]
-                for means in clf_results.values()
+            ) * 100
+    runs_recall_mean = sum(
+            means[0][2]
+            for means in clf_results.values()
+            ) / float(len(clf_results)) * 100
+    runs_recall_stdev = np.sqrt(
+            sum(vars[1][2]
+                for vars in clf_results.values()
                 ) / float(len(clf_results))
-        runs_recall_stdev = np.sqrt(
-                sum(vars[1][2]
-                    for vars in clf_results.values()
-                    ) / float(len(clf_results))
-                )
-        total_train_time = sum(
-                time[2][0] for time in clf_results.values()
-                )
-        total_test_time = sum(
-                time[2][1] for time in clf_results.values()
-                )
-        # Define summary row
-        summary = [
-                "SUMMARY",
-                clf_best_params if tune_time is not None else "default",
-                runs_accuracy_mean,
-                runs_accuracy_stdev,
-                runs_precision_mean,
-                runs_precision_stdev,
-                runs_recall_mean,
-                runs_recall_stdev,
-                total_train_time,
-                total_test_time,
-                clf_best_params if tune_time is not None else 0,
-                ]
-        # Write summary row
-        writer.writerow(summary)
+            ) * 100
+    total_train_time = sum(
+            time[2][0] for time in clf_results.values()
+            )
+    total_test_time = sum(
+            time[2][1] for time in clf_results.values()
+            )
+
+    # Define list of performance metrics to put in x-axis
+    metrics = ["Accuracy", "Precision", "Recall"]
+
+    # Build list of metrics mean and standard deviation
+    CTEs = [runs_accuracy_mean, runs_precision_mean, runs_recall_mean]
+    error = [runs_accuracy_stdev, runs_precision_stdev, runs_recall_stdev]
+
+    x_pos = np.arange(len(metrics))
+    y_pos = np.arange(0, 100 + 10, 10)
+
+    # Build annotation text to show precise performance metrics values
+    performance_text = ("\nAccuracy [%]: {:.2f} (±{:.2f})\n"
+                        "Precision [%]: {:.2f} (±{:.2f})\n"
+                        "Recall [%]: {:.2f} (±{:.2f})\n\n"
+                        "Train time [s]: {:.4f}\n"
+                        "Test time [s]: {:.4f}".format(
+                                runs_accuracy_mean, runs_accuracy_stdev,
+                                runs_precision_mean, runs_precision_stdev,
+                                runs_recall_mean, runs_recall_stdev,
+                                total_train_time, total_test_time
+                            )
+                        )
+    # Build bar plot
+    fig, ax = plt.subplots()
+    ax.bar(x_pos,
+           CTEs,
+           yerr=error,
+           align='center',
+           ecolor='black',
+           edgecolor='black',
+           capsize=5)
+    # Configure bar plot
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(metrics)
+    ax.set_xlabel(performance_text)
+    ax.set_ylabel("Score [%]")
+    ax.set_yticks(y_pos)
+
+    # Extract dirname from output path and
+    # create directory if it does not exist
+    output_dirname = Path(os.path.dirname(output_path))
+    output_dirname.mkdir(parents=True, exist_ok=True)
+    # Add png extension to output path
+    output_with_no_suffix = Path(output_path).with_suffix("")
+    output_with_png_suffix = Path(output_with_no_suffix).with_suffix(".png")
+
+    # Export and close plot
+    plt.tight_layout()
+    plt.savefig(output_with_png_suffix, bbox_inches="tight", dpi=400)
+    plt.close()
